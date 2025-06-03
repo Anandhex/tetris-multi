0e640124dfb8374647961f807d7ae22bea724377
Ramyad2000 <ramya20102000@gmail.com>
2025-06-03 22:36:15 +0200

    revertable commit

--- Assets/Scripts/InputControllers/PlacementInfo.cs
+++ Assets/Scripts/InputControllers/PlacementInfo.cs
@@ -1,9 +1,10 @@
 public class PlacementInfo
 {
-    public int linesCleared;
+ public int actionIndex;       public int linesCleared;
     public float aggregateHeight;
     public float maxHeight;
     public int holes;
+    public int landingRow;
     public float bumpiness;
     public float wellDepth;
     public int targetColumn;
